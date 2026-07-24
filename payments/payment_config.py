df5aebf8b130a70d4165a840fa66ca2142d97489
projectveridex <projectveridex@gmail.com>
2026-07-24 15:58:01 +0100

    Create payment_config.py with USDT network details
    Added payment configuration for USDT across multiple networks.

---
+++ payments/payment_config.py
@@ -0,0 +1,23 @@
+"""
+VERIDEX X
+PAYMENT CONFIG
+"""
+
+PAYMENT_METHODS = {
+
+    "USDT_TRC20": {
+        "network": "TRC20",
+        "address": "TNqdJvcH7115SbZDGfCeqrh35rfYsNPEtQ"
+    },
+
+    "USDT_BEP20": {
+        "network": "BEP20",
+        "address": "0xc79d3c5cd69faae2f818d770a304e8baa1b3944e"
+    },
+
+    "USDT_POLYGON": {
+        "network": "Polygon",
+        "address": "0xc79d3c5cd69faae2f818d770a304e8baa1b3944e"
+    }
+
+}
