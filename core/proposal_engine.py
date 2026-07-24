187537329f1272135214c0b9c84127d82ec72173
projectveridex <projectveridex@gmail.com>
2026-07-24 15:45:28 +0100

    Add proposal generation function in proposal_engine.py

---
+++ core/proposal_engine.py
@@ -0,0 +1,19 @@
+"""
+VERIDEX X
+Proposal Engine
+"""
+
+def generate_proposal(opportunity):
+
+    return f"""
+Hello,
+
+I can help you with:
+
+{opportunity.title}
+
+I can start immediately.
+
+Regards,
+VERIDEX X
+"""
