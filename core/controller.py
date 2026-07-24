5955f64cd489dc667c881b229bc28c6e4aea635d
projectveridex <projectveridex@gmail.com>
2026-07-24 15:46:02 +0100

    Implement master controller for Veridex

---
+++ core/controller.py
@@ -0,0 +1,58 @@
+"""
+VERIDEX X
+MASTER CONTROLLER
+"""
+
+from core.hunter_manager import run_hunters
+from core.duplicate_detector import remove_duplicates
+from core.proposal_engine import generate_proposal
+from core.learning_engine import LearningEngine
+from core.approval_queue import ApprovalQueue
+from core.task_planner import TaskPlanner
+from core.database import add_record
+
+learning = LearningEngine()
+approval_queue = ApprovalQueue()
+planner = TaskPlanner()
+
+LAST_SCAN = []
+
+
+def run_veridex():
+
+    global LAST_SCAN
+
+    opportunities = run_hunters()
+
+    opportunities = remove_duplicates(opportunities)
+
+    LAST_SCAN = opportunities
+
+    for opportunity in opportunities:
+
+        add_record({
+            "title": opportunity.title,
+            "source": opportunity.source
+        })
+
+        proposal = generate_proposal(opportunity)
+
+        approval_queue.add({
+            "opportunity": opportunity,
+            "proposal": proposal,
+            "plan": planner.plan(opportunity)
+        })
+
+        learning.record_submission()
+
+    return approval_queue.pending()
+
+
+def get_last_scan():
+
+    return LAST_SCAN
+
+
+def get_approval_queue():
+
+    return approval_queue
